5b2051cbb3d3064d334683fe779b0d19ddbc767d
FELIPE EDUARDO MORSCHEL <felipe.morschel@gmail.com>
2021-12-13 17:05:00 -0300

    Refactored resize to layout resizing

--- lib/src/rescale.dart
+++ lib/src/rescale.dart
@@ -1,28 +1,58 @@
 part of 'floating_overlay.dart';
 
 class _Rescale extends StatelessWidget {
-  const _Rescale({
+  _Rescale({
     Key? key,
     required this.child,
     required this.scaleController,
+    required this.childKey,
   }) : super(key: key);
 
   final Widget child;
   final _FloatingOverlayScale scaleController;
+  final GlobalKey childKey;
+  final cubit = _SizeCubit();
+
+  void function([void Function(Size)? lambda]) {
+    final box = childKey.currentContext!.findRenderObject()! as RenderBox;
+    lambda?.call(box.size);
+  }
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance?.addPostFrameCallback(
+      (_) => function((_size) => cubit.emit(_size)),
+    );
     return StreamBuilder<double>(
       initialData: scaleController.state,
       stream: scaleController.stream,
       builder: (context, snapshot) {
         final scale = snapshot.data!;
-        final vector = Vector3(scale, scale, 1.0);
-        return Transform(
-          transform: Matrix4.diagonal3(vector),
-          child: child,
+        return StreamBuilder<Size?>(
+          stream: cubit.stream,
+          builder: (context, snapshot) {
+            final size = snapshot.data;
+            if (size == null) {
+              return Offstage(
+                offstage: true,
+                child: child,
+              );
+            } else {
+              return SizedBox.fromSize(
+                size: size * scale,
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: child,
+                ),
+              );
+            }
+          },
         );
       },
     );
   }
+}
+
+class _SizeCubit extends Cubit<Size?> {
+  _SizeCubit([Size? initialState]) : super(initialState);
 }
