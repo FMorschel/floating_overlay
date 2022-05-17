4b850459635fb41cb6d13a5b56a9ff8e4de65dd6
FELIPE EDUARDO MORSCHEL <felipe.morschel@gmail.com>
2022-05-17 08:58:34 -0300

    Changing static class to enhanced enum

--- lib/src/cursor_border_side.dart
+++ lib/src/cursor_border_side.dart
@@ -1,7 +1,65 @@
 part of 'floating_overlay.dart';
 
-class _Side {
-  const _Side._({
+enum _Side {
+  left(
+    cursor: SystemMouseCursors.resizeLeftRight,
+    width: _defaultWidth,
+    leftDistance: 0,
+    topDistance: _defaultWidth,
+    bottomDistance: _defaultWidth,
+  ),
+  top(
+    cursor: SystemMouseCursors.resizeUpDown,
+    height: _defaultWidth,
+    topDistance: 0,
+    leftDistance: _defaultWidth,
+    rightDistance: _defaultWidth,
+  ),
+  right(
+    cursor: SystemMouseCursors.resizeLeftRight,
+    width: _defaultWidth,
+    rightDistance: 0,
+    topDistance: _defaultWidth,
+    bottomDistance: _defaultWidth,
+  ),
+  bottom(
+    cursor: SystemMouseCursors.resizeUpDown,
+    height: _defaultWidth,
+    bottomDistance: 0,
+    leftDistance: _defaultWidth,
+    rightDistance: _defaultWidth,
+  ),
+
+  topLeft(
+    cursor: SystemMouseCursors.resizeUpLeftDownRight,
+    width: _defaultWidth,
+    height: _defaultWidth,
+    leftDistance: 0,
+    topDistance: 0,
+  ),
+  topRight(
+    cursor: SystemMouseCursors.resizeUpRightDownLeft,
+    width: _defaultWidth,
+    height: _defaultWidth,
+    rightDistance: 0,
+    topDistance: 0,
+  ),
+  bottomLeft(
+    cursor: SystemMouseCursors.resizeUpRightDownLeft,
+    width: _defaultWidth,
+    height: _defaultWidth,
+    leftDistance: 0,
+    bottomDistance: 0,
+  ),
+  bottomRight(
+    cursor: SystemMouseCursors.resizeUpLeftDownRight,
+    width: _defaultWidth,
+    height: _defaultWidth,
+    rightDistance: 0,
+    bottomDistance: 0,
+  );
+
+  const _Side({
     required this.cursor,
     this.width,
     this.height,
@@ -10,63 +68,6 @@ class _Side {
     this.rightDistance,
     this.bottomDistance,
   });
-
-  static _Side get left => const _Side._(
-        cursor: SystemMouseCursors.resizeLeftRight,
-        width: _defaultWidth,
-        leftDistance: 0,
-        topDistance: _defaultWidth,
-        bottomDistance: _defaultWidth,
-      );
-  static _Side get top => const _Side._(
-        cursor: SystemMouseCursors.resizeUpDown,
-        height: _defaultWidth,
-        topDistance: 0,
-        leftDistance: _defaultWidth,
-        rightDistance: _defaultWidth,
-      );
-  static _Side get right => const _Side._(
-        cursor: SystemMouseCursors.resizeLeftRight,
-        width: _defaultWidth,
-        rightDistance: 0,
-        topDistance: _defaultWidth,
-        bottomDistance: _defaultWidth,
-      );
-  static _Side get bottom => const _Side._(
-        cursor: SystemMouseCursors.resizeUpDown,
-        height: _defaultWidth,
-        bottomDistance: 0,
-        leftDistance: _defaultWidth,
-        rightDistance: _defaultWidth,
-      );
-  static _Side get topLeft => const _Side._(
-        cursor: SystemMouseCursors.resizeUpLeftDownRight,
-        width: _defaultWidth,
-        height: _defaultWidth,
-        leftDistance: 0,
-        topDistance: 0,
-      );
-  static _Side get topRight => const _Side._(
-        cursor: SystemMouseCursors.resizeUpRightDownLeft,
-        width: _defaultWidth,
-        height: _defaultWidth,
-        rightDistance: 0,
-        topDistance: 0,
-      );
-  static _Side get bottomLeft => const _Side._(
-        cursor: SystemMouseCursors.resizeUpRightDownLeft,
-        width: _defaultWidth,
-        height: _defaultWidth,
-        leftDistance: 0,
-        bottomDistance: 0,
-      );
-  static _Side get bottomRight => const _Side._(
-        cursor: SystemMouseCursors.resizeUpLeftDownRight,
-        width: _defaultWidth,
-        height: _defaultWidth,
-        rightDistance: 0,
-        bottomDistance: 0,
-      );
 
   static const _defaultWidth = 3.0;
 
@@ -78,19 +79,17 @@ class _Side {
   final double? rightDistance;
   final double? bottomDistance;
 
+  bool get rotation => cursor == SystemMouseCursors.none;
+
   bool get diagonal {
     final primaria = cursor == SystemMouseCursors.resizeUpLeftDownRight;
     final secundaria = cursor == SystemMouseCursors.resizeUpRightDownLeft;
     return primaria || secundaria;
   }
 
-  bool get vertical {
-    return cursor == SystemMouseCursors.resizeUpDown;
-  }
+  bool get vertical => cursor == SystemMouseCursors.resizeUpDown;
 
-  bool get horizontal {
-    return cursor == SystemMouseCursors.resizeLeftRight;
-  }
+  bool get horizontal => cursor == SystemMouseCursors.resizeLeftRight;
 
   Offset consideringDelta(Offset delta) {
     if ((leftDistance == 0) && (delta.dx != 0)) {
